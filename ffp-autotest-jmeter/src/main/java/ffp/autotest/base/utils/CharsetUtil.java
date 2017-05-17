4e174289c25451dc8e6e400e7335eeefafc8644a
FanFengping <263869564@qq.com>
2017-05-17 21:22:28 +0800

    Add Charset util and common class.

---
+++ ffp-autotest-jmeter/src/main/java/ffp/autotest/base/utils/CharsetUtil.java
@@ -0,0 +1,58 @@
+/**
+ * Aaron.ffp Inc.
+ * Copyright (c) 2004-2017 All Rights Reserved.
+ */
+package ffp.autotest.base.utils;
+
+import java.nio.charset.Charset;
+
+import ffp.autotest.base.variables.CharsetVariable;
+
+/**
+ * <strong>字符编码工具类</strong><br><br>
+ * <br>
+ * @author FanFengping
+ * @version V1.0.0: ffp-autotest-base ffp.autotest.base.utils CharsetUtil.java, 2017-04-30 11:03:32 Exp $
+ */
+public class CharsetUtil {
+    public CharsetUtil() {
+    }
+    
+    /**
+     * <strong>设置编码为UTF-8，若编码为空</strong><br>
+     * 
+     * @author FanFengping
+     * @version V1.0.0: ffp-autotest-base ffp.autotest.base.utils CharsetUtil.java, 2017-04-30 11:04:12 Exp $
+     * 
+     * @param charset 编码
+     * @return
+     */
+    public static String setDefault(String charset) {
+        return StringUtil.isEmpty(charset) ? CharsetVariable.CHARSET_UTF8 : charset;
+    }
+    
+    /**
+     * <strong>获取默认编码</strong><br>
+     * 
+     * @author FanFengping
+     * @version V1.0.0: ffp-autotest-base ffp.autotest.base.utils CharsetUtil.java, 2017-04-30 11:01:43 Exp $
+     * 
+     * @return
+     */
+    public static String getDefault() {
+        return Charset.defaultCharset().name();
+    }
+    
+    /**
+     * <strong>验证编码是否合法</strong><br>
+     * 
+     * @author FanFengping
+     * @version V1.0.0: ffp-autotest-base ffp.autotest.base.utils CharsetUtil.java, 2017-04-30 11:01:04 Exp $
+     * 
+     * @param charset 编码
+     * @return
+     */
+    public static boolean valid(String charset) {
+        return StringUtil.isEmpty(charset) ? false : Charset.isSupported(charset);
+    }
+}
---
+++ ffp-autotest-jmeter/src/main/java/ffp/autotest/base/variables/CharsetVariable.java
@@ -0,0 +1,19 @@
+/**
+ * Aaron.ffp Inc.
+ * Copyright (c) 2017-2100 All Rights Reserved.
+ */
+package ffp.autotest.base.variables;
+
+/**
+ * <strong>字符编码类</strong><br><br>
+ * <br>
+ * @author FanFengping
+ * @version V1.0.0: ffp-autotest-base ffp.autotest.base.variables CharsetVariable.java, 2017年5月5日 上午12:01:01 Exp $
+ */
+public class CharsetVariable {
+    public static final String CHARSET_UTF8    = "UTF-8";
+    public static final String CHARSET_UTF16   = "UTF-16";
+    public static final String CHARSET_GBK     = "GBK";
+    public static final String CHARSET_GB2312  = "GB2312";
+    public static final String CHARSET_GB18030 = "GB18030";
+}
